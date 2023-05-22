1599ebcb4e149b4840d905285431737ba579104a
IVACHEV KONSTANTIN <kostya1.6@yandex.ru>
2023-05-22 15:56:22 +0500

    fix Страница не найдена

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const movieRouter = require('./movies');
-const { CustomError, NOT_FOUND } = require('../errors/errors');
+const { NOT_FOUND } = require('../errors/errors');
 
 router.use('/api/movies', movieRouter);
 router.use('/api', userRouter);
 router.use('*', (req, res, next) => {
-  next(new CustomError(NOT_FOUND, 'Страница не найдена'));
+  next(new NOT_FOUND('Страница не найдена'));
 });
 
 module.exports = router;
